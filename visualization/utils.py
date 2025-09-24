b4d0ab4c492e49162e335d9a44a08176f06606e4
Vilém Zouhar <vilem.zouhar@gmail.com>
2025-09-25 01:25:06 +0200

    rename systems

--- visualization/utils.py
+++ visualization/utils.py
@@ -2,17 +2,19 @@
 SYS_TO_NAME = {
     "CommandA_MT": r"CommandA\textsubscript{WMT}",
     "Systran_generic_ft": r"Systran-ft",
-    "organizers_gpt-4.1-nano": r"GPT-4.1-nano",
-    "organizers_NLLB": r"NLLB",
-    "organizers_MADLAD": r"MADLAD",
+    "organizers_gpt-4.1-nano": r"GPT-4.1-nano$\blacksquare$",
+    "organizers_NLLB": r"NLLB$\blacksquare$",
+    "organizers_MADLAD": r"MADLAD$\blacksquare$",
+    "stitch": "STITCH",
 }
 
 
 SYS_TO_NAME_2 = {
     "CommandA_MT": r"CommandA$_\mathrm{WMT}$",
     "Systran_generic_ft": r"Systran-ft",
-    "organizers_gpt-4.1-nano": r"GPT-4.1-nano",
-    "organizers_NLLB": r"NLLB",
-    "organizers_MADLAD": r"MADLAD",
+    "organizers_gpt-4.1-nano": r"GPT-4.1-nano$\blacksquare$",
+    "organizers_NLLB": r"NLLB$\blacksquare$",
+    "organizers_MADLAD": r"MADLAD$\blacksquare$",
+    "stitch": "STITCH",
 }
 
